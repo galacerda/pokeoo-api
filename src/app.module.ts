56b5bc8ebd4a33344c0d523b361fb2b44e2d9fd3
galacerda <galacerda260401@gmail.com>
2022-03-11 18:03:16 -0300

    ignore .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './features/pokemons/pokemon.module';
 import { ScheduleModule } from '@nestjs/schedule';
 
+const mongoDbUri = 'aa';
+
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://lacerda:19391945@mycluster-shard-00-00.0gx7b.mongodb.net:27017,mycluster-shard-00-01.0gx7b.mongodb.net:27017,mycluster-shard-00-02.0gx7b.mongodb.net:27017/pokeoo?replicaSet=atlas-y8l2u3-shard-0&ssl=true&authSource=admin',
-    ),
+    MongooseModule.forRoot(mongoDbUri),
     ScheduleModule.forRoot(),
     PokemonModule,
   ],
